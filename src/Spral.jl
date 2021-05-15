90bdb9c9dd9c3f2c2b5f9a6999c34d194b4ee038
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-05-15 01:11:52 -0400

    Add SPRAL_jll in the Project.toml

--- src/Spral.jl
+++ src/Spral.jl
@@ -2,7 +2,7 @@ module Spral
 
 using SPRAL_jll
 
-include("wrapper/qr_mumps_common.jl")
-include("wrapper/qr_mumps_api.jl")
+include("wrapper/spral_common.jl")
+include("wrapper/spral_api.jl")
 
 end # module
